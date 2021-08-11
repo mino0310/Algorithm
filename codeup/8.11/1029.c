1ae0a7baa067830c43ff80f3fe07665ce3b10aa0
mino0310 <bewriter310@gmail.com>
2021-08-12 06:58:56 +0900

    reorganizing source files

---
+++ codeup/8.11/1029.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	long double d;
+	scanf("%Lf", &d);
+	printf("%.11Lf", d);
+	return 0;
+}
---
+++ inflearn/Section2/41.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int cnt;
+
+int main(void)
+{
+    int n;
+
+    cin >> n;
+    
+
+    cout << cnt << endl;
+    return 0;
+}
