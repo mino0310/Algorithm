1806d2daabce7b8843de61561a7d2e68aa6ff17e
mino0310 <bewriter310@gmail.com>
2022-02-24 15:20:20 +0900

    inflearn algorithm resolved

--- inflearn_java/Section2_search/Mountain.java
+++ inflearn_java/Section2_search/Mountain.java
@@ -11,9 +11,9 @@ public class Mountain {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int size = Integer.parseInt(br.readLine());
-        int[][] map = new int[size + 2][size + 2];
+        int[][] map = new int[52][52];
         int[] dx = new int[]{-1, 0, 1, 0};
-        int[] dy = new int[]{0, -1, 0, 1};
+        int[] dy = new int[]{0, 1, 0, -1};
         int cnt = 0;
 
         for (int i = 1; i <= size; i++) {
@@ -26,22 +26,17 @@ public class Mountain {
         for (int i = 1; i <= size; i++) {
             for (int j = 1; j <= size; j++) {
                 int nowValue = map[i][j];
-//                System.out.println("now searching map["+i+"]["+j+"]");
-//                System.out.println("now value = " + nowValue);
                 boolean isBiggest = true;
                 for (int k = 0; k < 4; k++) {
-                    int newX = j + dx[k];
-                    int newY = i + dy[k];
-
-                    int searchedValue = map[newY][newX];
-//                    System.out.println("compared map[" +newY + "][" + newX + "] = " + searchedValue);
-                    if (searchedValue > nowValue) {
+                    int newX = i + dx[k];
+                    int newY = j + dy[k];
+                    int searchedValue = map[newX][newY];
+                    if (searchedValue >= nowValue) {
                         isBiggest = false;
                         break;
                     }
                 }
                 if (isBiggest) {
-                    System.out.println("it's biggest, value = " + map[i][j]);
                     cnt++;
                 }
             }
